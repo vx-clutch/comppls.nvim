56d6f7f3d00038a628347c66d5393489b5031f16
vx-clutch <owestness@gmail.com>
2024-09-27 10:06:14 -0400

    save

--- lua/comppls/init.lua
+++ lua/comppls/init.lua
@@ -1,4 +1,7 @@
 local M = {}
+M.setup = function ()
+	print("Your nvim is belong to us")
+end
 vim.api.nvim_create_user_command("Compile", function()
 	local cmd = vim.fn.input("Compile Command: ")
 	if cmd ~= "" then
