6bb13fc5edb0d517c612fbf47e4927e25a778d3e
vx-clutch <owestness@gmail.com>
2024-09-27 10:39:27 -0400

    save

--- lua/comppls/init.lua
+++ lua/comppls/init.lua
@@ -1,11 +1,16 @@
 local M = {}
-M.setup = function ()
+M.setup = function()
 	print("Your nvim is belong to us")
 end
 vim.api.nvim_create_user_command("Compile", function()
 	local cmd = vim.fn.input("Compile Command: ")
 	if cmd ~= "" then
-		print(cmd)
+		comp(cmd)
 	end
 end, {})
+function comp(cmd)
+	vim.cmd("sp")
+	local buf = vim.api.nvim_get_current_buf()
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, "Compilation started at " .. os.date("%a %b %X"))
+end
 return M
