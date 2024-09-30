9daf9dda595cee29ec06644414b7e1eccf036b2c
vx-clutch <owestness@gmail.com>
2024-09-30 15:00:14 -0400

    save

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ You want options? Too bad add them yourself
 
 use this is you want, more for me so like yeah
 use :CompPls or like bind to ig
+Or if you want to run shell commands in a similar style run :ShellPls
 
 Lazy.nvim
     {
--- lua/comppls/init.lua
+++ lua/comppls/init.lua
@@ -78,9 +78,87 @@ function M.compile()
 	end
 end
 
+function M.shell()
+	local last = ""
+
+	-- Function to split a string into a table of words
+	local function strTtab(input)
+		local result = {}
+		for word in string.gmatch(input, "%S+") do
+			table.insert(result, word)
+		end
+		return result
+	end
+
+	-- Function to split input by newlines into a table
+	local function strTtbl(input)
+		local result = {}
+		for line in string.gmatch(input, "[^\n]+") do
+			table.insert(result, line)
+		end
+		table.remove(result, 1)
+		return result
+	end
+
+	-- Prompt user for the compile command
+	local cmd = vim.fn.input("Shell Command: ", last)
+	last = cmd -- Store the last command for future use
+
+	if cmd ~= "" then
+		-- Create a new split window and buffer
+		vim.cmd("new")
+		local buf = vim.api.nvim_get_current_buf()
+		vim.api.nvim_buf_set_option(buf, "readonly", false)
+
+		-- Write the initial compilation message
+		local dir = vim.fn.getcwd()
+		vim.api.nvim_buf_set_lines(buf, 0, -1, false, { dir, "", cmd })
+
+		-- Execute the compilation command
+		local splitCmd = strTtab(cmd)
+		local result = vim.system(splitCmd, { text = true }):wait()
+
+		-- Check if the command failed (exit code is non-zero)
+		if result.code ~= 0 then
+			-- Display an error in the buffer and the command line
+			local errTbl = strTtbl(result.stderr)
+			vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "Error:", "" })
+			vim.api.nvim_buf_set_lines(buf, -1, -1, false, errTbl)
+			local errMsg = cmd .. " exited abnormaly with code " .. result.code .. " at " .. os.date("%a %B %d %X")
+			vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "", errMsg })
+
+			-- Notify the user
+			vim.cmd("echo")
+			vim.api.nvim_echo(
+				{ { cmd .. " failed: Invalid command or error during execution", "ErrorMsg" } },
+				false,
+				{}
+			)
+		else
+			-- Process the result if successful
+			local resultTbl = strTtbl(result.stdout)
+			vim.api.nvim_buf_set_lines(buf, -1, -1, false, resultTbl)
+			local okprt = cmd .. " finished at " .. os.date("%a %B %d %X")
+			vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "", okprt })
+		end
+
+		-- Highlight the result
+		vim.api.nvim_set_hl(0, "Good", { fg = "#00FF00" })
+		vim.api.nvim_set_hl(0, "Fatal", { fg = "#FF0000" })
+		vim.fn.matchadd("Good", "finished", 0, -1, { window = 0 })
+		vim.fn.matchadd("Fatal", "failed", 0, -1, { window = 0 })
+		vim.fn.matchadd("Fatal", "exited abnormaly", 0, -1, { window = 0 })
+
+		-- Set the buffer to readonly and mark it unmodified
+		vim.api.nvim_buf_set_option(buf, "readonly", true)
+		vim.api.nvim_buf_set_option(buf, "modified", false)
+	end
+end
+
 function M.setup()
 	-- Define CompPls command, calling compile logic
 	vim.api.nvim_create_user_command("CompPls", M.compile, {})
+	vim.api.nvim_create_user_command("ShellPls", M.shell, {})
 end
 
 return M
